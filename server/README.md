2fa4260bd72b218f2f0b7c21d62091023a9593b5
Dayan Mitev <daqn.mitev7@gmail.com>
2021-06-07 12:06:14 +0300

    Final commit

--- server/README.md
+++ server/README.md
@@ -1,3 +1,1 @@
 ### This is the backend for my multiplayer chess game.
-
-Link to the frontend: [Frontend](https://github.com/ProjectsByJackHe/multiplayer-chess-game)
--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,22 @@ import React, {useRef, useState } from 'react'
 import { Form, Button, Card, Alert } from 'react-bootstrap'
 import { useAuth } from './context/AuthContext'
 import { Link, useHistory } from 'react-router-dom'
+import "./styles/stylesLogin.css"
+
+
+
+import one from "./styles/img/falling_figures/1.png";
+import two from "./styles/img/falling_figures/2.png";
+import three from "./styles/img/falling_figures/3.png";
+import four from "./styles/img/falling_figures/4.png";
+import five from "./styles/img/falling_figures/5.png";
+import six from "./styles/img/falling_figures/6.png";
+import a from "./styles/img/falling_figures/a.png";
+import b from "./styles/img/falling_figures/b.png";
+import c from "./styles/img/falling_figures/c.png";
+import d from "./styles/img/falling_figures/d.png";
+import e from "./styles/img/falling_figures/1.png";
+import f from "./styles/img/falling_figures/1.png";
 
 export default function Login() {
     const emailRef = useRef();
@@ -50,6 +66,34 @@ export default function Login() {
             <div className="w-100 text-center mt-2">
                 Need an account? <Link to="/signup">Sign Up</Link>
             </div>
+            <div class='set'>
+                <div><img src={one}/></div>
+                <div><img src={f}/></div>
+                <div><img src={two}/></div>
+                <div><img src={e}/></div>
+                <div><img src={three}/></div>
+                <div><img src={d}/></div>
+                <div><img src={four}/></div>
+                <div><img src={c}/></div>
+                <div><img src={five}/></div>
+                <div><img src={b}/></div>
+                <div><img src={six}/></div>
+                <div><img src={a}/></div>
         </div>
+        <div class='set set2'>
+            <div><img src={one}/></div>
+            <div><img src={f}/></div>
+            <div><img src={two}/></div>
+            <div><img src={e}/></div>
+            <div><img src={three}/></div>
+            <div><img src={d}/></div>
+            <div><img src={four}/></div>
+            <div><img src={c}/></div>
+            <div><img src={five}/></div>
+            <div><img src={b}/></div>
+            <div><img src={six}/></div>
+            <div><img src={a}/></div>
+        </div>
+    </div>
     )
 }
